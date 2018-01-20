4d3d51979a32802c291fd47a26fc5afe2ef0d0cb
ducviencse <ducvien.cse@gmail.com>
2018-01-21 00:43:48 +0700

    delete abundant file

--- app/src/main/java/com/navigate/reminderlazier/AddReminder.java
+++ app/src/main/java/com/navigate/reminderlazier/AddReminder.java
@@ -23,7 +23,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.navigate.reminderlazier.fragments.TimePickerFragment;
 
 import java.util.Calendar;
 import java.util.Date;
